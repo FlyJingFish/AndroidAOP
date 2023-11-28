bf1cbb8594c05487c2928b2d4c4cf10fad98f689
tianxiangyu <96164429+FlyJingFish@users.noreply.github.com>
2023-11-28 15:53:52 +0800

    移动代码

--- android-aop-plugin/src/main/kotlin/com/flyjingfish/android_aop_plugin/AssembleAndroidAopTask.kt
+++ android-aop-plugin/src/main/kotlin/com/flyjingfish/android_aop_plugin/AssembleAndroidAopTask.kt
@@ -1,19 +1,16 @@
 package com.flyjingfish.android_aop_plugin
 
-import com.flyjingfish.android_aop_plugin.scanner_visitor.AnnotationMethodScanner
-import com.flyjingfish.android_aop_plugin.scanner_visitor.AnnotationScanner
-import com.flyjingfish.android_aop_plugin.utils.Utils.MethodAnnoUtils
 import com.flyjingfish.android_aop_plugin.beans.ClassMethodRecord
 import com.flyjingfish.android_aop_plugin.beans.MethodRecord
-import com.flyjingfish.android_aop_plugin.beans.WovenInfo
+import com.flyjingfish.android_aop_plugin.scanner_visitor.AnnotationMethodScanner
+import com.flyjingfish.android_aop_plugin.scanner_visitor.AnnotationScanner
 import com.flyjingfish.android_aop_plugin.scanner_visitor.RegisterMapWovenInfoCode
 import com.flyjingfish.android_aop_plugin.scanner_visitor.WovenIntoCode
 import com.flyjingfish.android_aop_plugin.utils.AndroidConfig
-import com.flyjingfish.android_aop_plugin.utils.ClassPoolUtils
+import com.flyjingfish.android_aop_plugin.utils.InitConfig
 import com.flyjingfish.android_aop_plugin.utils.Utils
+import com.flyjingfish.android_aop_plugin.utils.Utils.MethodAnnoUtils
 import com.flyjingfish.android_aop_plugin.utils.WovenInfoUtils
-import com.google.gson.Gson
-import com.google.gson.GsonBuilder
 import org.gradle.api.DefaultTask
 import org.gradle.api.file.Directory
 import org.gradle.api.file.RegularFile
@@ -28,9 +25,6 @@ import java.io.ByteArrayInputStream
 import java.io.File
 import java.io.FileInputStream
 import java.io.FileOutputStream
-import java.io.IOException
-import java.nio.file.Files
-import java.nio.file.Paths
 import java.util.jar.JarEntry
 import java.util.jar.JarFile
 import java.util.jar.JarOutputStream
@@ -54,78 +48,9 @@ abstract class AssembleAndroidAopTask : DefaultTask() {
         private const val END_NAME = "\$\$AndroidAopClass.class"
         private const val _CLASS = ".class"
     }
-    private lateinit var temporaryDir: File
-    private lateinit var buildConfigCacheFile: File
-    private val gson: Gson = GsonBuilder().create()
-    fun <T> optFromJsonString(jsonString: String, clazz: Class<T>): T? {
-        try {
-            return gson.fromJson(jsonString, clazz)
-        } catch (e: Throwable) {
-            logger.warn("optFromJsonString(${jsonString}, $clazz", e)
-        }
-        return null
-    }
 
-    fun optToJsonString(any: Any): String {
-        try {
-            return gson.toJson(any)
-        } catch (throwable: Throwable) {
-            logger.warn("optToJsonString(${any}", throwable)
-        }
-        return ""
-    }
-
-    fun loadBuildConfig(buildConfigCacheFile: File): WovenInfo {
-        return if (buildConfigCacheFile.exists()) {
-            val jsonString = readAsString(buildConfigCacheFile.absolutePath)
-            optFromJsonString(jsonString, WovenInfo::class.java) ?: WovenInfo()
-        } else {
-            WovenInfo()
-        }
-    }
-    fun saveBuildConfig() {
-        val wovenInfo = WovenInfo()
-        wovenInfo.aopMatchCuts = WovenInfoUtils.aopMatchCuts
-        wovenInfo.aopMethodCuts = WovenInfoUtils.aopMethodCuts
-        wovenInfo.classPaths = WovenInfoUtils.classPaths
-        saveFile(buildConfigCacheFile, optToJsonString(wovenInfo))
-    }
-    fun saveFile(file: File,data:String) {
-        val fos = FileOutputStream(file.absolutePath)
-        try {
-            fos.write(data.toByteArray())
-        } catch (e:IOException) {
-            e.printStackTrace();
-        }finally {
-            fos.close()
-        }
-    }
-    public fun readAsString(path :String) :String {
-        return try {
-            val content = String(Files.readAllBytes(Paths.get(path)));
-            content
-        }catch (exception:Exception){
-            ""
-        }
-    }
     @TaskAction
     fun taskAction() {
-        temporaryDir = File(project.buildDir.absolutePath+"/tmp")
-        buildConfigCacheFile = File(temporaryDir, "buildAndroidAopConfigCache.json")
-//        val wovenInfo = loadBuildConfig(buildConfigCacheFile)
-//        if(wovenInfo.aopMatchCuts != null){
-//            WovenInfoUtils.aopMatchCuts = wovenInfo.aopMatchCuts!!
-//        }
-//        if(wovenInfo.aopMethodCuts != null){
-//            WovenInfoUtils.aopMethodCuts = wovenInfo.aopMethodCuts!!
-//        }
-//        if(wovenInfo.classPaths != null){
-//            WovenInfoUtils.classPaths = wovenInfo.classPaths!!
-//        }
-
-//        logger.error("buildConfigCacheFile = ${buildConfigCacheFile.absolutePath}")
-//        logger.error("buildConfig = $wovenInfo")
-
         jarOutput = JarOutputStream(BufferedOutputStream(FileOutputStream(output.get().asFile)))
         val scanTimeCost = measureTimeMillis {
             scanFile()
@@ -136,7 +61,13 @@ abstract class AssembleAndroidAopTask : DefaultTask() {
     }
 
     private fun scanFile() {
-        val androidConfig : AndroidConfig = AndroidConfig(project)
+        loadJoinPointConfig()
+        searchJoinPointLocation()
+        wovenIntoCode()
+    }
+
+    private fun loadJoinPointConfig(){
+        val androidConfig = AndroidConfig(project)
         val list: List<File> = androidConfig.getBootClasspath()
         logger.info("Scan to classPath [${list}]")
         for (file in list) {
@@ -192,10 +123,11 @@ abstract class AssembleAndroidAopTask : DefaultTask() {
             jarFile.close()
         }
 
-        saveBuildConfig()
+//        InitConfig.saveBuildConfig()
 //        ClassPoolUtils.initClassPool()
-//        logger.error("第二遍找到要注入代码的类---------")
-        //第二遍找配置有注解的类和方法
+    }
+
+    private fun searchJoinPointLocation(){
         allDirectories.get().forEach { directory ->
             directory.asFile.walk().forEach { file ->
                 if (file.isFile) {
@@ -259,7 +191,9 @@ abstract class AssembleAndroidAopTask : DefaultTask() {
             }
             jarFile.close()
         }
-//        logger.error("第三遍---------")
+    }
+
+    private fun wovenIntoCode(){
         allDirectories.get().forEach { directory ->
 //            logger.error("Scan to directory1 [${directory.asFile.absolutePath}]")
             directory.asFile.walk().forEach { file ->
---
+++ android-aop-plugin/src/main/kotlin/com/flyjingfish/android_aop_plugin/utils/InitConfig.kt
@@ -0,0 +1,97 @@
+package com.flyjingfish.android_aop_plugin.utils
+
+import com.android.build.gradle.internal.coverage.JacocoReportTask
+import com.flyjingfish.android_aop_plugin.beans.WovenInfo
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import org.gradle.api.Project
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
+import java.nio.file.Files
+import java.nio.file.Paths
+
+
+object InitConfig {
+    private lateinit var temporaryDir: File
+    private lateinit var buildConfigCacheFile: File
+    var isInit: Boolean = false
+    private val gson: Gson = GsonBuilder().create()
+    fun <T> optFromJsonString(jsonString: String, clazz: Class<T>): T? {
+        try {
+            return gson.fromJson(jsonString, clazz)
+        } catch (e: Throwable) {
+            JacocoReportTask.JacocoReportWorkerAction.logger.warn("optFromJsonString(${jsonString}, $clazz", e)
+        }
+        return null
+    }
+
+    fun optToJsonString(any: Any): String {
+        try {
+            return gson.toJson(any)
+        } catch (throwable: Throwable) {
+            JacocoReportTask.JacocoReportWorkerAction.logger.warn("optToJsonString(${any}", throwable)
+        }
+        return ""
+    }
+
+    fun loadBuildConfig(buildConfigCacheFile: File): WovenInfo {
+        return if (buildConfigCacheFile.exists()) {
+            val jsonString = readAsString(buildConfigCacheFile.absolutePath)
+            optFromJsonString(jsonString, WovenInfo::class.java) ?: WovenInfo()
+        } else {
+            WovenInfo()
+        }
+    }
+    fun saveBuildConfig() {
+        val wovenInfo = WovenInfo()
+        wovenInfo.aopMatchCuts = WovenInfoUtils.aopMatchCuts
+        wovenInfo.aopMethodCuts = WovenInfoUtils.aopMethodCuts
+        wovenInfo.classPaths = WovenInfoUtils.classPaths
+        saveFile(buildConfigCacheFile, optToJsonString(wovenInfo))
+    }
+    fun saveFile(file: File, data:String) {
+        val fos = FileOutputStream(file.absolutePath)
+        try {
+            fos.write(data.toByteArray())
+        } catch (e: IOException) {
+            e.printStackTrace();
+        }finally {
+            fos.close()
+        }
+    }
+    fun readAsString(path :String) :String {
+        return try {
+            val content = String(Files.readAllBytes(Paths.get(path)));
+            content
+        }catch (exception:Exception){
+            ""
+        }
+    }
+
+    fun init(project: Project):Boolean{
+        temporaryDir = File(project.buildDir.absolutePath+"/tmp")
+        buildConfigCacheFile = File(temporaryDir, "buildAndroidAopConfigCache.json")
+        val wovenInfo = loadBuildConfig(buildConfigCacheFile)
+        var count =0
+        if(wovenInfo.aopMatchCuts != null){
+            WovenInfoUtils.aopMatchCuts = wovenInfo.aopMatchCuts!!
+            count++
+        }
+        if(wovenInfo.aopMethodCuts != null){
+            WovenInfoUtils.aopMethodCuts = wovenInfo.aopMethodCuts!!
+            count++
+        }
+        if(wovenInfo.classPaths != null){
+            WovenInfoUtils.classPaths = wovenInfo.classPaths!!
+            count++
+        }
+
+        JacocoReportTask.JacocoReportWorkerAction.logger.error("buildConfigCacheFile = ${buildConfigCacheFile.absolutePath}")
+        JacocoReportTask.JacocoReportWorkerAction.logger.error("buildConfig = $wovenInfo")
+        isInit = count == 3
+        return isInit
+    }
+
+
+}
