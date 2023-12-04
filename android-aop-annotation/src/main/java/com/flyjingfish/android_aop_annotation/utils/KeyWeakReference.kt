2614868c4ee5f07ebd5028893422fd00ab26fd7c
FlyJingFish <tianxiangyu168@gmail.com>
2023-12-04 22:33:00 +0800

    1、防止内存泄漏

--- android-aop-annotation/src/main/java/com/flyjingfish/android_aop_annotation/utils/KeyWeakReference.kt
+++ android-aop-annotation/src/main/java/com/flyjingfish/android_aop_annotation/utils/KeyWeakReference.kt
@@ -3,6 +3,4 @@ package com.flyjingfish.android_aop_annotation.utils
 import java.lang.ref.ReferenceQueue
 import java.lang.ref.WeakReference
 
-class KeyWeakReference<T>(referent: T, q: ReferenceQueue<in T>,val key:String) : WeakReference<T>(referent, q) {
-
-}
+class KeyWeakReference<T>(referent: T, q: ReferenceQueue<in T>,val key:String) : WeakReference<T>(referent, q)
