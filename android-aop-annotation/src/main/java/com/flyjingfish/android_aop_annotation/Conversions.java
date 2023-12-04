a390b244b84a6bbc7ef15bba0d5a05b4d7348160
FlyJingFish <tianxiangyu168@gmail.com>
2023-12-04 23:39:32 +0800

    1、删除多余代码

--- android-aop-annotation/src/main/java/com/flyjingfish/android_aop_annotation/Conversions.java
+++ android-aop-annotation/src/main/java/com/flyjingfish/android_aop_annotation/Conversions.java
@@ -1,21 +1,4 @@
-/* *******************************************************************
- * Copyright (c) 1999-2001 Xerox Corporation,
- *               2002 Palo Alto Research Center, Incorporated (PARC).
- * All rights reserved.
- * This program and the accompanying materials are made available
- * under the terms of the Eclipse Public License v 2.0
- * which accompanies this distribution and is available at
- * https://www.eclipse.org/org/documents/epl-2.0/EPL-2.0.txt
- *
- * Contributors:
- *     Xerox/PARC     initial implementation
- * ******************************************************************/
-
-
 package com.flyjingfish.android_aop_annotation;
-
-import java.util.HashMap;
-import java.util.Map;
 
 public final class Conversions {
 	// Can't make instances of me
