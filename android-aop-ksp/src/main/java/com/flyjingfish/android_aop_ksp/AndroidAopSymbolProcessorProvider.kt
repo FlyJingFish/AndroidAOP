7824b665c249738eeb6a09c6632821428d6c6350
tianxiangyu <96164429+FlyJingFish@users.noreply.github.com>
2023-12-07 18:56:36 +0800

    1、修改依赖配置

--- android-aop-ksp/src/main/java/com/flyjingfish/android_aop_ksp/AndroidAopSymbolProcessorProvider.kt
+++ android-aop-ksp/src/main/java/com/flyjingfish/android_aop_ksp/AndroidAopSymbolProcessorProvider.kt
@@ -1,10 +1,8 @@
 package com.flyjingfish.android_aop_ksp
-import com.google.auto.service.AutoService
 import com.google.devtools.ksp.processing.SymbolProcessor
 import com.google.devtools.ksp.processing.SymbolProcessorEnvironment
 import com.google.devtools.ksp.processing.SymbolProcessorProvider
 
-@AutoService(SymbolProcessorProvider::class)
 class AndroidAopSymbolProcessorProvider : SymbolProcessorProvider {
   override fun create(environment: SymbolProcessorEnvironment): SymbolProcessor {
     return AndroidAopSymbolProcessor(environment.codeGenerator, environment.logger)
