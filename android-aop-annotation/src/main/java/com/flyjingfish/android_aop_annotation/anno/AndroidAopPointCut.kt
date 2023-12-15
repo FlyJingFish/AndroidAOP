89bb99e0fcf8572f1884e5f98a749cb71bc18a08
FlyJingFish <tianxiangyu168@gmail.com>
2023-12-15 23:35:16 +0800

    1、修改为kotlin代码

---
+++ android-aop-annotation/src/main/java/com/flyjingfish/android_aop_annotation/anno/AndroidAopPointCut.kt
@@ -0,0 +1,18 @@
+package com.flyjingfish.android_aop_annotation.anno
+
+import com.flyjingfish.android_aop_annotation.base.BasePointCut
+import kotlin.reflect.KClass
+
+
+/**
+ * 定义注解切面的注解，使用这个注解的类需要是注解类
+ */
+@Target(AnnotationTarget.ANNOTATION_CLASS)
+@Retention(AnnotationRetention.SOURCE)
+annotation class AndroidAopPointCut(
+    /**
+     *
+     * @return 处理切面的类
+     */
+    val value: KClass<out BasePointCut<out Annotation>>
+)
