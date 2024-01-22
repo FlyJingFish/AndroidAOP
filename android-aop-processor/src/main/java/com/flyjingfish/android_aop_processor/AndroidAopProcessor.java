8d9a62001a4264f18e348d5617d8c8d3f7d9045c
FlyJingFish <tianxiangyu168@gmail.com>
2024-01-23 00:10:22 +0800

    1、升级版本

--- android-aop-processor/src/main/java/com/flyjingfish/android_aop_processor/AndroidAopProcessor.java
+++ android-aop-processor/src/main/java/com/flyjingfish/android_aop_processor/AndroidAopProcessor.java
@@ -37,7 +37,7 @@ import javax.lang.model.util.Types;
 
 public class AndroidAopProcessor extends AbstractProcessor {
     Filer mFiler;
-    private static final String packageName = "com.flyjingfish.android_aop_core.aop";
+//    private static final String packageName = "com.flyjingfish.android_aop_core.aop";
     private TypeMirror matchClassMethodType;
     private Types types;
 
@@ -83,119 +83,115 @@ public class AndroidAopProcessor extends AbstractProcessor {
 
     public void processPointCut(Set<? extends TypeElement> set, RoundEnvironment roundEnvironment) {
         Set<? extends Element> elements = roundEnvironment.getElementsAnnotatedWith(AndroidAopPointCut.class);
-        for (TypeElement typeElement: set){
-            Name name = typeElement.getSimpleName();
-            for (Element element : elements) {
-                Name name1 = element.getSimpleName();
-                AndroidAopPointCut cut = element.getAnnotation(AndroidAopPointCut.class);
-                Target target = element.getAnnotation(Target.class);
-                Retention retention = element.getAnnotation(Retention.class);
-                if (target == null){
-                    throw new IllegalArgumentException("注意：" + "请给 "+name1+" 设置 @Target 为ElementType.METHOD ");
-                }
-                ElementType[] elementTypes = target.value();
-                if (elementTypes.length > 1){
-                    throw new IllegalArgumentException("注意：" + name1 + "只可以设置 @Target 为 ElementType.METHOD 这一种");
-                }else if (elementTypes.length == 1){
-                    if (elementTypes[0] != ElementType.METHOD){
-                        throw new IllegalArgumentException("注意：" + "请给 "+name1+" 设置 @Target 为 ElementType.METHOD ");
-                    }
-                }else {
+        for (Element element : elements) {
+            Name name1 = element.getSimpleName();
+            AndroidAopPointCut cut = element.getAnnotation(AndroidAopPointCut.class);
+            Target target = element.getAnnotation(Target.class);
+            Retention retention = element.getAnnotation(Retention.class);
+            if (target == null){
+                throw new IllegalArgumentException("注意：" + "请给 "+name1+" 设置 @Target 为ElementType.METHOD ");
+            }
+            ElementType[] elementTypes = target.value();
+            if (elementTypes.length > 1){
+                throw new IllegalArgumentException("注意：" + name1 + "只可以设置 @Target 为 ElementType.METHOD 这一种");
+            }else if (elementTypes.length == 1){
+                if (elementTypes[0] != ElementType.METHOD){
                     throw new IllegalArgumentException("注意：" + "请给 "+name1+" 设置 @Target 为 ElementType.METHOD ");
                 }
-                if (retention == null){
-                    throw new IllegalArgumentException("注意：" + "请给 "+name1+" 设置 @Retention 为 RetentionPolicy.RUNTIME ");
-                }
-                RetentionPolicy retentionPolicy = retention.value();
-                if (retentionPolicy == null){
-                    throw new IllegalArgumentException("注意：" + "请给 "+name1+" 设置 @Retention 为 RetentionPolicy.RUNTIME ");
-                }else if (retentionPolicy != RetentionPolicy.RUNTIME){
-                    throw new IllegalArgumentException("注意：" + "必须给 "+name1+" 设置 @Retention 为 RetentionPolicy.RUNTIME ");
-                }
+            }else {
+                throw new IllegalArgumentException("注意：" + "请给 "+name1+" 设置 @Target 为 ElementType.METHOD ");
+            }
+            if (retention == null){
+                throw new IllegalArgumentException("注意：" + "请给 "+name1+" 设置 @Retention 为 RetentionPolicy.RUNTIME ");
+            }
+            RetentionPolicy retentionPolicy = retention.value();
+            if (retentionPolicy == null){
+                throw new IllegalArgumentException("注意：" + "请给 "+name1+" 设置 @Retention 为 RetentionPolicy.RUNTIME ");
+            }else if (retentionPolicy != RetentionPolicy.RUNTIME){
+                throw new IllegalArgumentException("注意：" + "必须给 "+name1+" 设置 @Retention 为 RetentionPolicy.RUNTIME ");
+            }
 
-                String className;
-                try{
-                    className = cut.value().getName();
-                }catch (MirroredTypeException mirroredTypeException){
-                    String errorMessage = mirroredTypeException.getLocalizedMessage();
-                    className = errorMessage.substring( errorMessage.lastIndexOf(" ")+1);
-                }
-                TypeSpec.Builder typeBuilder = TypeSpec.classBuilder(name1+"$$AndroidAopClass")
-                        .addAnnotation(AndroidAopClass.class)
-                        .addModifiers(Modifier.PUBLIC, Modifier.FINAL);
-                MethodSpec.Builder whatsMyName1 = whatsMyName("withinAnnotatedClass")
-                        .addAnnotation(AnnotationSpec.builder(AndroidAopMethod.class)
-                                .addMember("value", "$S", "@"+element)
-                                .addMember("pointCutClassName", "$S", className)
-                                .build());
+            String className;
+            try{
+                className = cut.value().getName();
+            }catch (MirroredTypeException mirroredTypeException){
+                String errorMessage = mirroredTypeException.getLocalizedMessage();
+                className = errorMessage.substring( errorMessage.lastIndexOf(" ")+1);
+            }
+            TypeSpec.Builder typeBuilder = TypeSpec.classBuilder(name1+"$$AndroidAopClass")
+                    .addAnnotation(AndroidAopClass.class)
+                    .addModifiers(Modifier.PUBLIC, Modifier.FINAL);
+            MethodSpec.Builder whatsMyName1 = whatsMyName("withinAnnotatedClass")
+                    .addAnnotation(AnnotationSpec.builder(AndroidAopMethod.class)
+                            .addMember("value", "$S", "@"+element)
+                            .addMember("pointCutClassName", "$S", className)
+                            .build());
 
-                typeBuilder.addMethod(whatsMyName1.build());
+            typeBuilder.addMethod(whatsMyName1.build());
 
-                TypeSpec typeSpec = typeBuilder.build();
-
-                JavaFile javaFile = JavaFile.builder(packageName, typeSpec)
-                        .build();
-                try {
-                    javaFile.writeTo(mFiler);
-                } catch (IOException e) {
-//                    throw new RuntimeException(e);
-                }
+            TypeSpec typeSpec = typeBuilder.build();
+            String elementClassName = element.toString();
+            String packageName = elementClassName.substring(0, elementClassName.lastIndexOf("."));
+            JavaFile javaFile = JavaFile.builder(packageName, typeSpec)
+                    .build();
+            try {
+                javaFile.writeTo(mFiler);
+            } catch (IOException e) {
+//                throw new RuntimeException(e);
             }
         }
     }
 
     public void processMatch(Set<? extends TypeElement> set, RoundEnvironment roundEnvironment) {
         Set<? extends Element> elements = roundEnvironment.getElementsAnnotatedWith(AndroidAopMatchClassMethod.class);
-        for (TypeElement typeElement: set){
-            Name name = typeElement.getSimpleName();
-            for (Element element : elements) {
-                if (!types.isSubtype(element.asType(), matchClassMethodType)) {
-                    throw new IllegalArgumentException("注意：" +element+ " 必须实现 MatchClassMethod 接口");
-                }
-                Name name1 = element.getSimpleName();
+        for (Element element : elements) {
+            if (!types.isSubtype(element.asType(), matchClassMethodType)) {
+                throw new IllegalArgumentException("注意：" +element+ " 必须实现 MatchClassMethod 接口");
+            }
+            Name name1 = element.getSimpleName();
 //                System.out.println("======"+element);
-                AndroidAopMatchClassMethod cut = element.getAnnotation(AndroidAopMatchClassMethod.class);
-                String className = cut.targetClassName();
-                String[] methodNames = cut.methodName();
-                String[] excludeClasses = cut.excludeClasses();
-                MatchType matchType = cut.type();
-                StringBuilder methodNamesBuilder = new StringBuilder();
-                for (int i = 0; i < methodNames.length; i++) {
-                    methodNamesBuilder.append(methodNames[i]);
-                    if (i != methodNames.length -1){
-                        methodNamesBuilder.append("-");
-                    }
+            AndroidAopMatchClassMethod cut = element.getAnnotation(AndroidAopMatchClassMethod.class);
+            String className = cut.targetClassName();
+            String[] methodNames = cut.methodName();
+            String[] excludeClasses = cut.excludeClasses();
+            MatchType matchType = cut.type();
+            StringBuilder methodNamesBuilder = new StringBuilder();
+            for (int i = 0; i < methodNames.length; i++) {
+                methodNamesBuilder.append(methodNames[i]);
+                if (i != methodNames.length -1){
+                    methodNamesBuilder.append("-");
                 }
-                StringBuilder excludeClassesBuilder = new StringBuilder();
-                for (int i = 0; i < excludeClasses.length; i++) {
-                    excludeClassesBuilder.append(excludeClasses[i]);
-                    if (i != excludeClasses.length -1){
-                        excludeClassesBuilder.append("-");
-                    }
+            }
+            StringBuilder excludeClassesBuilder = new StringBuilder();
+            for (int i = 0; i < excludeClasses.length; i++) {
+                excludeClassesBuilder.append(excludeClasses[i]);
+                if (i != excludeClasses.length -1){
+                    excludeClassesBuilder.append("-");
                 }
-                TypeSpec.Builder typeBuilder = TypeSpec.classBuilder(name1+"$$AndroidAopClass")
-                        .addAnnotation(AndroidAopClass.class)
-                        .addModifiers(Modifier.PUBLIC, Modifier.FINAL);
-                MethodSpec.Builder whatsMyName1 = whatsMyName("withinAnnotatedClass")
-                        .addAnnotation(AnnotationSpec.builder(AndroidAopMatch.class)
-                                .addMember("baseClassName", "$S", className)
-                                .addMember("methodNames", "$S", methodNamesBuilder)
-                                .addMember("pointCutClassName", "$S", element)
-                                .addMember("matchType", "$S", matchType.name())
-                                .addMember("excludeClasses", "$S", excludeClassesBuilder)
-                                .build());
+            }
+            TypeSpec.Builder typeBuilder = TypeSpec.classBuilder(name1+"$$AndroidAopClass")
+                    .addAnnotation(AndroidAopClass.class)
+                    .addModifiers(Modifier.PUBLIC, Modifier.FINAL);
+            MethodSpec.Builder whatsMyName1 = whatsMyName("withinAnnotatedClass")
+                    .addAnnotation(AnnotationSpec.builder(AndroidAopMatch.class)
+                            .addMember("baseClassName", "$S", className)
+                            .addMember("methodNames", "$S", methodNamesBuilder)
+                            .addMember("pointCutClassName", "$S", element)
+                            .addMember("matchType", "$S", matchType.name())
+                            .addMember("excludeClasses", "$S", excludeClassesBuilder)
+                            .build());
 
-                typeBuilder.addMethod(whatsMyName1.build());
+            typeBuilder.addMethod(whatsMyName1.build());
 
-                TypeSpec typeSpec = typeBuilder.build();
-
-                JavaFile javaFile = JavaFile.builder(packageName, typeSpec)
-                        .build();
-                try {
-                    javaFile.writeTo(mFiler);
-                } catch (IOException e) {
-//                    throw new RuntimeException(e);
-                }
+            TypeSpec typeSpec = typeBuilder.build();
+            String elementClassName = element.toString();
+            String packageName = elementClassName.substring(0, elementClassName.lastIndexOf("."));
+            JavaFile javaFile = JavaFile.builder(packageName, typeSpec)
+                    .build();
+            try {
+                javaFile.writeTo(mFiler);
+            } catch (IOException e) {
+//                throw new RuntimeException(e);
             }
         }
     }
