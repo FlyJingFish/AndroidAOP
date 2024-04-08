68fd4cd1ff6766d00a0c20088ac655d22a80cb6e
tianxiangyu <96164429+FlyJingFish@users.noreply.github.com>
2024-04-08 16:51:49 +0800

    1、去掉切面处理类的反射部分

--- android-aop-annotation/src/main/java/com/flyjingfish/android_aop_annotation/base/MatchClassMethodCreator.kt
+++ android-aop-annotation/src/main/java/com/flyjingfish/android_aop_annotation/base/MatchClassMethodCreator.kt
@@ -1,10 +1,5 @@
 package com.flyjingfish.android_aop_annotation.base
 
-import com.flyjingfish.android_aop_annotation.anno.AndroidAopMatchClassMethod
-
-/**
- * 匹配切面的回调接口与 [AndroidAopMatchClassMethod] 配合使用
- */
 interface MatchClassMethodCreator {
     fun newInstance(): MatchClassMethod
 }
