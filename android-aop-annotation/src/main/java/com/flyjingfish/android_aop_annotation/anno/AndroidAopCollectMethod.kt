f80b535e7a368b185b0e4b8d6d25f4f7e84af32a
tianxiangyu <96164429+FlyJingFish@users.noreply.github.com>
2024-05-10 17:52:55 +0800

    1、完善新增的切面类型

--- android-aop-annotation/src/main/java/com/flyjingfish/android_aop_annotation/anno/AndroidAopCollectMethod.kt
+++ android-aop-annotation/src/main/java/com/flyjingfish/android_aop_annotation/anno/AndroidAopCollectMethod.kt
@@ -1,13 +1,11 @@
 package com.flyjingfish.android_aop_annotation.anno
 
 /**
- * 定义收集类
+ * 定义收集类切面
  * [wiki 文档使用说明](https://github.com/FlyJingFish/AndroidAOP/wiki/@AndroidAopCollectMethod)
  */
 @Target(
-    AnnotationTarget.FUNCTION,
-    AnnotationTarget.PROPERTY_GETTER,
-    AnnotationTarget.PROPERTY_SETTER
+    AnnotationTarget.FUNCTION
 )
 @Retention(AnnotationRetention.BINARY)
 annotation class AndroidAopCollectMethod
