b7e224312e1e07b6525b3f4fdbc42eace29e9df3
tianxiangyu <96164429+FlyJingFish@users.noreply.github.com>
2024-07-16 11:32:04 +0800

    1、完善代码

--- android-aop-annotation/src/main/java/com/flyjingfish/android_aop_annotation/base/OnBaseSuspendReturnListener.kt
+++ android-aop-annotation/src/main/java/com/flyjingfish/android_aop_annotation/base/OnBaseSuspendReturnListener.kt
@@ -1,9 +1,3 @@
 package com.flyjingfish.android_aop_annotation.base
 
-import com.flyjingfish.android_aop_annotation.ProceedJoinPointSuspend
-
-/**
- * 对于 suspend 修饰的函数，可通过这个接口修改返回值
- * 这个类只能在 [ProceedJoinPointSuspend] 中设置
- */
-interface OnBaseSuspendReturnListener
+sealed interface OnBaseSuspendReturnListener
