7c2907a22f929c09ba06dc2f2763ca7f161b4fea
tianxiangyu <96164429+FlyJingFish@users.noreply.github.com>
2024-07-16 15:59:32 +0800

    1、完善代码

--- android-aop-core/src/main/java/com/flyjingfish/android_aop_core/utils/SetHandler.java
+++ android-aop-core/src/main/java/com/flyjingfish/android_aop_core/utils/SetHandler.java
@@ -3,11 +3,18 @@ package com.flyjingfish.android_aop_core.utils;
 import android.os.Handler;
 import android.os.Looper;
 
+
 import com.flyjingfish.android_aop_annotation.utils.HandlerUtils;
 
 class SetHandler {
     static {
         Handler handler = new Handler(Looper.getMainLooper());
-        HandlerUtils.INSTANCE.setHandler(handler::post);
+        HandlerUtils.INSTANCE.setHandler(runnable -> {
+            if (Looper.myLooper() == Looper.getMainLooper()){
+                runnable.run();
+            }else {
+                handler.post(runnable);
+            }
+        });
     }
 }
