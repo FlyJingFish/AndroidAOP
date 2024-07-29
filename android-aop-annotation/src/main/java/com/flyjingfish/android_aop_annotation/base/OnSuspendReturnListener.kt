fb73835f07192399a2a767cf2c8c8f2a1bc8482a
tianxiangyu <96164429+FlyJingFish@users.noreply.github.com>
2024-07-29 18:29:49 +0800

    1、优化代码

--- android-aop-annotation/src/main/java/com/flyjingfish/android_aop_annotation/base/OnSuspendReturnListener.kt
+++ android-aop-annotation/src/main/java/com/flyjingfish/android_aop_annotation/base/OnSuspendReturnListener.kt
@@ -8,7 +8,7 @@ import com.flyjingfish.android_aop_annotation.ProceedReturn2
  * 对于 suspend 修饰的函数，可通过这个接口修改返回值
  * 这个类只能在 [ProceedJoinPointSuspend] 中设置
  */
-interface OnSuspendReturnListener : OnBaseSuspendReturnListener{
+fun interface OnSuspendReturnListener : OnBaseSuspendReturnListener{
     /**
      * 此处的返回值就是 suspend 函数的返回值
      */
--- android-aop-annotation/src/main/java/com/flyjingfish/android_aop_annotation/base/OnSuspendReturnListener2.kt
+++ android-aop-annotation/src/main/java/com/flyjingfish/android_aop_annotation/base/OnSuspendReturnListener2.kt
@@ -7,7 +7,7 @@ import com.flyjingfish.android_aop_annotation.ProceedReturn2
  * 对于 suspend 修饰的函数，可通过这个接口修改返回值
  * 这个类只能在 [ProceedJoinPointSuspend] 中设置
  */
-interface OnSuspendReturnListener2 : OnBaseSuspendReturnListener{
+fun interface OnSuspendReturnListener2 : OnBaseSuspendReturnListener{
     /**
      * 此处的返回值就是 suspend 函数的返回值
      */
