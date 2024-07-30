5e75490e6a82af25cbaa230a41203ddc34a035f5
FlyJingFish <tianxiangyu168@gmail.com>
2024-07-30 09:55:52 +0800

    1、修改注释

--- android-aop-annotation/src/main/java/com/flyjingfish/android_aop_annotation/base/MatchClassMethodSuspend.kt
+++ android-aop-annotation/src/main/java/com/flyjingfish/android_aop_annotation/base/MatchClassMethodSuspend.kt
@@ -11,6 +11,11 @@ import com.flyjingfish.android_aop_annotation.anno.AndroidAopMatchClassMethod
 interface MatchClassMethodSuspend : MatchClassMethod {
     /**
      * 匹配到的被 suspend 修饰的方法被调用时将会回调这个方法,不会回调 [invoke]
+     *
+     * 并且注意最好在实现方法内使用 withContext 等函数包裹您的代码，否则可能有Bug
+     *
+     * [详细使用说明看这里](https://github.com/FlyJingFish/AndroidAOP/wiki/Suspend-%E5%88%87%E7%82%B9%E5%87%BD%E6%95%B0#2%E6%94%AF%E6%8C%81-suspend-%E7%9A%84-basepointcutsuspend-%E5%92%8C-matchclassmethodsuspend)
+     *
      * @param joinPoint 切点相关信息
      * @param methodName 匹配的方法名，如果是 Lambda 表达式，请看 wiki 文档
      */
