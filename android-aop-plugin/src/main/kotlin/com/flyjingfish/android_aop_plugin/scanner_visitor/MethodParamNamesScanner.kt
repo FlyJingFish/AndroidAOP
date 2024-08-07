7090160a41ccd50ef75342368777bc5add1ae48e
tianxiangyu <96164429+FlyJingFish@users.noreply.github.com>
2024-08-07 18:23:01 +0800

    1、修复参数名获取失败的问题

---
+++ android-aop-plugin/src/main/kotlin/com/flyjingfish/android_aop_plugin/scanner_visitor/MethodParamNamesScanner.kt
@@ -0,0 +1,71 @@
+package com.flyjingfish.android_aop_plugin.scanner_visitor
+
+import org.objectweb.asm.ClassReader
+import org.objectweb.asm.tree.ClassNode
+import org.objectweb.asm.tree.MethodNode
+import java.util.Arrays
+
+
+class MethodParamNamesScanner(inputStreamBytes: ByteArray) {
+    private val methods: List<MethodNode>
+
+    init {
+        val cr = ClassReader(inputStreamBytes)
+        val cn = ClassNode()
+        cr.accept(cn, ClassReader.EXPAND_FRAMES) // 建议EXPAND_FRAMES
+        // ASM树接口形式访问
+        val methods = cn.methods
+        this.methods = methods
+    }
+
+    /**
+     * 获取参数名列表辅助方法
+     *
+     * @param name
+     * @param desc
+     * @param size
+     * @return
+     */
+    fun getParamNames(
+        name: String,
+        desc: String,
+        size: Int
+    ): List<String> {
+        val list: MutableList<String> = ArrayList()
+        for (i in methods.indices) {
+            val varNames: MutableList<LocalVariable> = ArrayList()
+            val method = methods[i]
+            // 验证方法签名
+            if (method.desc == desc && method.name == name) {
+                val localVariables = method.localVariables
+                for (l in localVariables.indices) {
+                    val varName = localVariables[l].name
+                    // index-记录了正确的方法本地变量索引。(方法本地变量顺序可能会被打乱。而index记录了原始的顺序)
+                    val index = localVariables[l].index
+                    if ("this" != varName) // 非静态方法,第一个参数是this
+                        varNames.add(LocalVariable(index, varName))
+                }
+                val tmpArr = varNames.toTypedArray()
+                // 根据index来重排序，以确保正确的顺序
+                Arrays.sort(tmpArr)
+                for (j in 0 until size) {
+                    list.add(tmpArr[j].name)
+                }
+                break
+            }
+        }
+        return list
+    }
+
+    /**
+     * 方法本地变量索引和参数名封装
+     * @author xby Administrator
+     */
+    private class LocalVariable(var index: Int, var name: String) :
+        Comparable<LocalVariable> {
+        override fun compareTo(o: LocalVariable): Int {
+            return index - o.index
+        }
+    }
+
+}
