a25be9577d88840a84d0c3496360252082995a2f
tianxiangyu <96164429+FlyJingFish@users.noreply.github.com>
2024-09-06 10:40:00 +0800

    完善库

--- android-aop-annotation/src/main/java/com/flyjingfish/android_aop_annotation/utils/AndroidAOPDebugUtils.kt
+++ android-aop-annotation/src/main/java/com/flyjingfish/android_aop_annotation/utils/AndroidAOPDebugUtils.kt
@@ -1,7 +1,7 @@
 package com.flyjingfish.android_aop_annotation.utils
 
 object AndroidAOPDebugUtils {
-    fun init(){
+    internal fun init(){
         Class.forName("com.flyjingfish.android_aop_core.utils.AndroidAOPInit")
     }
     var isDebug : Boolean = false
