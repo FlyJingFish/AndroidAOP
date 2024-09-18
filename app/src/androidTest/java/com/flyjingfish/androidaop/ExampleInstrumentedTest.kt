54a04a15d5fa1db6f333d92049c0d4dd2bbf0255
tianxiangyu <96164429+FlyJingFish@users.noreply.github.com>
2024-09-18 14:53:18 +0800

    新增测试代码

---
+++ app/src/androidTest/java/com/flyjingfish/androidaop/ExampleInstrumentedTest.kt
@@ -0,0 +1,32 @@
+package com.flyjingfish.androidaop
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.rule.ActivityTestRule
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+import org.junit.Rule
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @get:Rule
+    val activityRule = ActivityTestRule(MainActivity::class.java)
+
+    @Test
+    fun testButtonClick() {
+        // 点击按钮
+        onView(withId(R.id.btn_SingleClick)).perform(click())
+
+    }
+}
