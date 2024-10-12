dcccd70b350621b3f20edea6da335424c0ce6977
tianxiangyu <96164429+FlyJingFish@users.noreply.github.com>
2024-10-12 18:02:33 +0800

    1、完善切换语言

---
+++ docs/give_list.md
@@ -0,0 +1,10 @@
+## Donation List
+
+Your stars and support are my biggest motivation to move forward!
+
+| Nickname | Date | Method | Message | Links | Amount of Reward |
+|--|:----------:|:----:|:--------------------:|:----:|:------:|
+| O*e | 2024-04-11 |  Wechat  | aop解决了我的问题，请你喝杯瑞[咖啡] |  /   | ¥ 9.99 |
+| j*e | 2024-04-11 |  Wechat  |     辛苦大佬这么晚帮我找问题     |  /   |  ¥ 88  |
+| *张 | 2024-07-03 |  Wechat  |          /           |  /   |  ¥ 64.6  |
+| *君 | 2024-09-19 |  Wechat  |          /           |  /   |  ¥ 66   |
---
+++ docs/js/language-switch.js
@@ -0,0 +1,36 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const languageSelectors = document.querySelectorAll(".md-select__item");
+
+    languageSelectors.forEach(selector => {
+        selector.addEventListener("click", function () {
+            var target = event.target || event.srcElement;      // 兼容处理
+            if (target.nodeName.toLocaleLowerCase() === "a") {    // 判断是否匹配目标元素
+                const selectedLang = target.getAttribute("hreflang");
+                let currentPath = window.location.pathname;
+                // console.log('currentPath='+currentPath);
+                // console.log('selectedLang='+selectedLang);
+                event.preventDefault();
+                // 获取当前语言前缀，例如 "/zh/" 或 "/en/"
+                const isZh = currentPath.includes("/zh/");
+				// console.log('isZh='+isZh);
+				
+                if (isZh) {//在中文网页
+					if(selectedLang == "en"){
+						window.location.href = currentPath.replace('/zh/', '/');
+					}else{
+						window.location.href = currentPath;
+					}
+                }else{//英文网页
+                	if(selectedLang == "zh"){
+                		window.location.href = currentPath.replace('/AndroidAOP/', '/AndroidAOP/zh/');
+                	}else{
+						window.location.href = currentPath;
+                		
+                	}
+                }
+            }
+
+
+        });
+    });
+});
