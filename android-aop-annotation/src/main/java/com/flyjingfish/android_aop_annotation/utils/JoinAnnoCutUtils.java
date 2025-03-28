860d28cc6d7dda33f2ac34ca1332df0de43421aa
tianxiangyu <96164429+FlyJingFish@users.noreply.github.com>
2025-03-28 16:20:23 +0800

    完善逻辑

--- android-aop-annotation/src/main/java/com/flyjingfish/android_aop_annotation/utils/JoinAnnoCutUtils.java
+++ android-aop-annotation/src/main/java/com/flyjingfish/android_aop_annotation/utils/JoinAnnoCutUtils.java
@@ -11,37 +11,42 @@ import java.util.concurrent.ConcurrentHashMap;
 final class JoinAnnoCutUtils {
     private static final Map<String, BasePointCutCreator> mAnnoCutBeanMap = new ConcurrentHashMap<>();
     private static final Map<String, MatchClassMethodCreator> mAnnoMatchBeanMap = new ConcurrentHashMap<>();
+
     static {
         registerCreators();
         registerMatchCreators();
         registerDebugAndroidAopInfo();
     }
+
     private static void registerDebugAndroidAopInfo() {
-        if (mAnnoCutBeanMap.isEmpty()){
+        if (mAnnoCutBeanMap.isEmpty() || mAnnoMatchBeanMap.isEmpty()) {
             try {
                 Class.forName("com.flyjingfish.android_aop_annotation.utils.DebugAndroidAopInit");
             } catch (ClassNotFoundException ignored) {
             }
         }
     }
+
     private static void registerMatchCreators() {
     }
 
-    private static void registerCreators(){
+    private static void registerCreators() {
 
     }
 
-    static void registerCreator(String className,BasePointCutCreator cutCreator){
-        mAnnoCutBeanMap.put(className,cutCreator);
-    }
-    public static BasePointCutCreator getCutClassCreator(String className){
-        return mAnnoCutBeanMap.get("@"+className);
+    static void registerCreator(String className, BasePointCutCreator cutCreator) {
+        mAnnoCutBeanMap.put(className, cutCreator);
     }
 
-    static void registerMatchCreator(String className,MatchClassMethodCreator cutCreator){
-        mAnnoMatchBeanMap.put(className,cutCreator);
+    public static BasePointCutCreator getCutClassCreator(String className) {
+        return mAnnoCutBeanMap.get("@" + className);
     }
-    public static MatchClassMethodCreator getMatchClassCreator(String className){
+
+    static void registerMatchCreator(String className, MatchClassMethodCreator cutCreator) {
+        mAnnoMatchBeanMap.put(className, cutCreator);
+    }
+
+    public static MatchClassMethodCreator getMatchClassCreator(String className) {
         return mAnnoMatchBeanMap.get(className);
     }
 
